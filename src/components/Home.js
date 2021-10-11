8cf49010c40c732af06e5c2e72fcdb3821270a78
sidehustler <singhdhananjay125@outlook.com>
2021-10-11 18:14:27 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,13 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import "./styles/Home.css";
+import userPic from "../assets/user.png"
+
 function Home() {
   const [post, setPost] = useState([]);
+  const [onfilter, setOnfilter] = useState(0);
+  const [myTags, setMyTags] = useState("bp");
+
   useEffect(async () => {
     await axios
       .get("https://video-api-dot-dj-virtual-spaces.el.r.appspot.com/")
@@ -28,17 +33,27 @@ function Home() {
         <p>All Videos</p>
       </div>
       <div className="video-container">
+
         {post.map((data, index) => {
           return (
             <div className="video-card">
               <img src={data.thumbnailUrl} className="card-img" />
-
               <h1>{data.tags}</h1>
               <p className="card-title">{data.title}</p>
             </div>
           );
-        })}
+        })} 
+        
       </div>
+      <div className="image-container">
+        <img className="image__img" src={userPic} alt="back-image" />
+        <div className="image__overlay">
+          <div className="image__title">Title</div>
+          <p className="image__description">I am Description</p>
+        </div>
+
+      </div>
+      
     </div>
   );
 }
