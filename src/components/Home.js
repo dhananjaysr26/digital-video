2418584911f9cbf4915db3acb5f876650391f617
sidehustler <singhdhananjay125@outlook.com>
2021-10-12 14:33:34 +0530

    Feat: Added Filtering Post on the Basis of Tags

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,22 @@ import MyModalVideo from "./MyModalVideo";
 
 function Home() {
   const [post, setPost] = useState([]);
+  const [filterPost, setFilterPost] = useState([]);
   const [onfilter, setOnfilter] = useState(0);
   const [myIndex, setMyIndex] = useState([]);
+
+  const filterItems=(mytags)=>{
+    const myPost=[];
+    filterPost.filter((curElem)=>{
+      var Xr=[...curElem.tags]
+      if(Xr.includes(mytags)){
+          myPost.push(curElem)
+        }
+      }
+    )
+    setPost(myPost)
+  }
+
   function myfav(index) {
     if (index in myIndex) {
       return 1;
@@ -16,36 +30,36 @@ function Home() {
       return 0;
     }
   }
-
   useEffect(async () => {
     await axios
       .get("https://video-api-dot-dj-virtual-spaces.el.r.appspot.com/")
       .then((res) => {
         setPost(res.data.videosData);
+        setFilterPost(res.data.videosData)
         console.log(res.data.videosData);
       });
-  }, []);
+  }, [onfilter]);
   return (
     <div className="home">
       <div className="home-top"></div>
       <div className="home-tags">
         <span>Tags:</span>
         <ul>
-          <li>Favorites</li>
-          <li>CT Scans</li>
-          <li>DRTB</li>
-          <li>Neurology</li>
+        <li onClick={()=>onfilter?setOnfilter(0):setOnfilter(1)}>All</li>
+          <li onClick={()=>alert("Your Fevorite Video will be Shown!")}>Favorites</li>
+          <li onClick={()=>filterItems("tb")}>TB</li>
+          <li onClick={()=>filterItems("asthma")}>Asthma</li>
+          <li onClick={()=>filterItems("cancer")} >Cancer</li>
         </ul>
       </div>
       <div className="heading">
         <p>All Videos</p>
       </div>
       <div className="video-container">
-        {post.map((data, index) => {
+       {post.map((data, index) => {
           let videoUrl = data.videolink;
           let videoIDArray = videoUrl.split("/");
           let videoID = videoIDArray[4];
-
           return (
             <div className="video-card">
               <img src={data.thumbnailUrl} className="image__img" />
